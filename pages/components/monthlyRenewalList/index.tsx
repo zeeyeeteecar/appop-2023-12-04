df25154fb01fb5f6c8175b2770dc4eff671d80f1
zeeyeeteecar <tomtangjob@gmail.com>
2023-01-20 10:38:54 -0800

    1

--- pages/components/monthlyRenewalList/index.tsx
+++ pages/components/monthlyRenewalList/index.tsx
@@ -36,40 +36,83 @@ export default function Index() {
   }, []);
   console.log(fetchData);
 
+  const formatDate = (dateFormatNeeded: Date) => {
+    const expiryYear = new Date(dateFormatNeeded).getFullYear();
+    const expiryMonth = new Date(dateFormatNeeded).getUTCMonth() + 1;
+    const expiryDate = new Date(dateFormatNeeded).getUTCDate();
+    //console.log("expiryYear", expiryYear);
+    return expiryYear + "-" + expiryMonth + "-" + expiryDate;
+  };
+
   return (
-    <>
-      <Table variant="striped" colorScheme="teal">
-        <TableCaption>Imperial to metric conversion factors</TableCaption>
+    <VStack>
+      <HStack>
+        <Text></Text>
+      </HStack>
+      <Table variant="striped" colorScheme="gray">
+        <TableCaption>Monthly PP Renewal </TableCaption>
         <Thead>
           <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
+            <Th w="40px">User ID</Th>
+            <Th>F name</Th>
+            <Th>L name</Th>
+            <Th w="150px">addressLine1</Th>
+            <Th>city</Th>
+            <Th w="40px">prov</Th>
+            <Th>post</Th>
+            <Th w="100px">Expiry Date</Th>
+            <Th>PP #</Th>
+            <Th>Area</Th>
+            <Th>phone</Th>
+            <Th>email</Th>
+            <Th>phone</Th>
           </Tr>
         </Thead>
         <Tbody>
-          
-            {fetchData &&
-              fetchData.map((applicant, index) => {
-                //console.log("Performer_id==", item.Performer_id);
-                return (
-                  <Tr key={index}>
-                    <Td>{applicant.id}</Td>
-                    <Td>{applicant.firstName}</Td>
-                    <Td>{applicant.lastName}</Td>
-                  </Tr>
-                );
-              })}
-          
+          {fetchData &&
+            fetchData.map((applicant, index) => {
+              //console.log("Performer_id==", item.Performer_id);
+
+              const sortedPermits = [...applicant.permits].sort(
+                (a, b) => b.rcdPermitId - a.rcdPermitId
+              );
+              const latestPermitExpiryDate = formatDate(
+                sortedPermits[0].expiryDate
+              );
+              const latestPermitID = sortedPermits[0].rcdPermitId;
+              const areaCode = applicant.phone.substring(0, 3);
+              const phone =
+                applicant.phone.substring(3, 3 + 3) +
+                "-" +
+                applicant.phone.substring(3 + 3, 3 + 3 + 4);
+
+              return (
+                <Tr key={index}>
+                  <Td>{applicant.id}</Td>
+                  <Td>{applicant.firstName}</Td>
+                  <Td>{applicant.lastName}</Td>
+                  <Td w="250px">{applicant.addressLine1}</Td>
+                  <Td>{applicant.city}</Td>
+                  <Td>{applicant.province}</Td>
+                  <Td>{applicant.postalCode}</Td>
+                  <Td>{latestPermitExpiryDate}</Td>
+                  <Td>{latestPermitID}</Td>
+                  <Td>{areaCode}</Td>
+                  <Td>{phone}</Td>
+                  <Td>{applicant.email}</Td>
+                  <Td>{applicant.lastName}</Td>
+                  {sortedPermits.map((permit, index) => {
+                    return (
+                      <>
+                        <Td w={"100px"}>{formatDate(permit.expiryDate)}</Td>
+                      </>
+                    );
+                  })}
+                </Tr>
+              );
+            })}
         </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th>To convert</Th>
-            <Th>into</Th>
-            <Th isNumeric>multiply by</Th>
-          </Tr>
-        </Tfoot>
       </Table>
-    </>
+    </VStack>
   );
 }
