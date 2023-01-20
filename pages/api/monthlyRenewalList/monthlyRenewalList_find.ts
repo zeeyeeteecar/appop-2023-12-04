797deb6e3113731d836a4ce6564a07fc07fcecd1
zeeyeeteecar <tomtangjob@gmail.com>
2023-01-19 20:25:46 -0800

    1

---
+++ pages/api/monthlyRenewalList/monthlyRenewalList_find.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../prisma";
+//import { PrismaClient } from "@prisma/client";
+//const prisma = new PrismaClient();
+
+export default async function handle(req: any, res: any) {
+  //const prisma = new PrismaClient();
+  //await prisma.$connect();
+
+  const result = await prisma.applicant.findMany({
+    
+    
+     include: {
+        permits: true,
+     },
+    orderBy: [
+      {
+        id: "asc",
+      },
+    ],
+  });
+
+  // await prisma.$disconnect();
+  // res.json(result);
+
+  // const result = await prisma.performers.findMany({
+  //   include: {
+  //     donors: true,
+  //   },
+  //   orderBy: { Performer_id: "asc" },
+  // });
+
+  await prisma.$disconnect();
+  res.json(result);
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
-import React, { useEffect } from "react";
-import { useRouter } from "next/router";
-
-export default function Home() {
-  // const router = useRouter();
-
-  // React.useEffect(() => {
-  //   router.push("/components/monthlyRenewalList");
-  // });
-
+export default function index() {
   return (
     <ul>
       <li>
-        <Link href="/components/monthlyRenewalList">monthlyRenewalLis</Link>
+        <Link href="/components/monthlyRenewalList">Home</Link>
       </li>
       <li>
         <Link href="/about">About Us</Link>
@@ -24,5 +14,5 @@ export default function Home() {
         <Link href="/blog/hello-world">Blog Post</Link>
       </li>
     </ul>
-  );
+  )
 }
