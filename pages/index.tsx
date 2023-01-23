30cab9b829b7ba3648c0a9d47f5856b6f04bc979
zeeyeeteecar <tomtangjob@gmail.com>
2023-01-23 12:30:16 -0800

    6

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import {
   Center,
+  Box,
   VStack,
   Button,
   HStack,
@@ -14,106 +15,114 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
+import { url } from "inspector";
+import img from "./images/bg_coffee.jpg";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <Center borderWidth={"0px"} height={"100vh"} bgColor="green.50">
-      <VStack w="500px" borderWidth={"0px"} height={"500px"} spacing={10}>
-        <VStack
-          height={"100vh"}
-          width="100%"
-          bgColor="green.50"
-          spacing={5}
-          p="10"
-          boxShadow={"2xl"}
-          bg={useColorModeValue("white", "gray.700")}
-          rounded={"xl"}
-          align={"center"}
+    <Box
+      id="imgbox"
+      borderWidth={"0px"}
+      height={"100vh"}
+      width="100%"
+      bgImage={"/bg_coffee.jpg"}
+      bgPos="center"
+      bgSize={"cover"}
+    >
+      <VStack
+        w="900px"
+        borderWidth={"0px"}
+        height={"500px"}
+        // bgColor="green.50"
+        p="60px"
+        spacing={10}
+        // boxShadow={"2xl"}
+        // bg={useColorModeValue("white", "gray.700")}
+        rounded={"xl"}
+        align={"center"}
+      >
+        <Button
+          onClick={() => router.push("/components/monthlyRenewalList")}
+          fontSize="20px"
+          rounded={"full"}
+          w="300px"
+          h="60px"
+          bgColor="blackAlpha.300"
+          color="white"
+          border={"1px"}
+          borderColor={"gray.600"}
+          _hover={{
+            background: "gray.600",
+            color: "white",
+          }}
+
+          // bgGradient={"linear(to-r, green.50,green.150)"}
         >
-          <Button
-            onClick={() => router.push("/components/monthlyRenewalList")}
-            fontSize="20px"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-          >
-            Monthly PP Renewal List
-          </Button>
+          Monthly PP Renewal List
+        </Button>
 
-          <Button
-            onClick={() => router.push("/components/doctorInfo")}
-            size="lg"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-            fontSize="20px"
-            value={"Doctor Info"}
-          >
-            Doctor Info
-          </Button>
+        <Button
+          onClick={() => router.push("/components/doctorInfo")}
+          size="lg"
+          rounded={"full"}
+          w="300px"
+          h="60px"
+          bgColor="blackAlpha.300"
+          color="white"
+          border={"1px"}
+          borderColor={"gray.600"}
+          _hover={{
+            background: "gray.600",
+            color: "white",
+          }}
+          // bgGradient={"linear(to-r, green.50,green.150)"}
+          fontSize="20px"
+          value={"Doctor Info"}
+        >
+          Doctor Info
+        </Button>
 
-          <Button
-            onClick={() => router.push("/components/walletCard")}
-            size="lg"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-            fontSize="20px"
-            value={"Wallet Card"}
-          >
-            Wallet Card
-          </Button>
+        <Button
+          onClick={() => router.push("/components/walletCard")}
+          size="lg"
+          rounded={"full"}
+          w="300px"
+          h="60px"
+          bgColor="blackAlpha.300"
+          color="white"
+          border={"1px"}
+          borderColor={"gray.600"}
+          _hover={{
+            background: "gray.600",
+            color: "white",
+          }}
+          // bgGradient={"linear(to-r, green.50,green.150)"}
+          fontSize="20px"
+          value={"Wallet Card"}
+        >
+          Wallet Card
+        </Button>
 
-          <Button
-            onClick={() => router.push("#")}
-            size="lg"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-            fontSize="20px"
-            value={""}
-          ></Button>
-
-          <Button
-            onClick={() => router.push("#")}
-            size="lg"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-            fontSize="20px"
-            value={""}
-          ></Button>
-
-          <Button
-            onClick={() => router.push("#")}
-            size="lg"
-            rounded={"full"}
-            w="300px"
-            h="50px"
-            bgColor="green.100"
-            color="green"
-            bgGradient={"linear(to-r, green.50,green.150)"}
-            fontSize="20px"
-            value={""}
-          ></Button>
-        </VStack>
+        <Button
+          onClick={() => router.push("#")}
+          size="lg"
+          rounded={"full"}
+          w="300px"
+          h="60px"
+          bgColor="blackAlpha.300"
+          color="white"
+          border={"1px"}
+          borderColor={"gray.600"}
+          _hover={{
+            background: "gray.600",
+            color: "white",
+          }}
+          // bgGradient={"linear(to-r, green.50,green.150)"}
+          fontSize="20px"
+          value={""}
+        ></Button>
       </VStack>
-    </Center>
+    </Box>
   );
 }
