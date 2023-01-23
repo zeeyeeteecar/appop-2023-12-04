36960daa5762eafdceb7964e72cb70973f6b4908
zeeyeeteecar <tomtangjob@gmail.com>
2023-01-23 11:07:46 -0800

    8

--- pages/components/walletCard/index.tsx
+++ pages/components/walletCard/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { jsPDF } from "jspdf";
 
-export default function WalletCard() {
-  const doc = new jsPDF();
-  doc.text("Hello world!", 10, 10);
-  doc.save("a4.pdf"); // will save the file in the current working directory
 
+
+
+export default function WalletCard() {
+
+
+  const doc = new jsPDF();
+  doc.text("Hello world!", 50, 200);
+ // doc.save("a4.pdf"); // will save the file in the current working directory
+  console.log("tet13");
   return <div>const doc = new jsPDF;</div>;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,67 +11,107 @@ import {
   Flex,
   Radio,
   RadioGroup,
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
-  Tr,
-  Th,
-  Td,
-  TableCaption,
-  TableContainer,
+  useColorModeValue,
 } from "@chakra-ui/react";
 import { EditIcon } from "@chakra-ui/icons";
 
 export default function Index() {
   const router = useRouter();
   return (
-    <Center borderWidth={"0px"} height={"100vh"}>
-      <VStack w="100%" borderWidth={"0px"} spacing={10} height={"500px"}>
-        <VStack height={"100vh"} width="100%" bgColor="green.50" spacing={5} p="10">
+    <Center borderWidth={"0px"} height={"100vh"} bgColor="green.50">
+      <VStack w="500px" borderWidth={"0px"} height={"500px"} spacing={10}>
+        <VStack
+          height={"100vh"}
+          width="100%"
+          bgColor="green.50"
+          spacing={5}
+          p="10"
+          boxShadow={"2xl"}
+          bg={useColorModeValue("white", "gray.700")}
+          rounded={"xl"}
+          align={"center"}
+        >
           <Button
             onClick={() => router.push("/components/monthlyRenewalList")}
-            size="lg"
-            w="400px"
-            h="70px"
-            colorScheme={"teal"}
-            fontSize="30px"
+            fontSize="20px"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
           >
             Monthly PP Renewal List
           </Button>
+
           <Button
             onClick={() => router.push("/components/doctorInfo")}
             size="lg"
-            w="400px"
-            h="70px"
-            colorScheme={"teal"}
-            fontSize="30px"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
+            fontSize="20px"
             value={"Doctor Info"}
           >
             Doctor Info
           </Button>
+
           <Button
             onClick={() => router.push("/components/walletCard")}
             size="lg"
-            w="400px"
-            h="70px"
-            colorScheme={"teal"}
-            fontSize="30px"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
+            fontSize="20px"
             value={"Wallet Card"}
           >
             Wallet Card
           </Button>
+
           <Button
             onClick={() => router.push("#")}
             size="lg"
-            w="400px"
-            h="70px"
-            colorScheme={"teal"}
-            fontSize="30px"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
+            fontSize="20px"
             value={""}
-          >
-           
-          </Button>
+          ></Button>
+
+          <Button
+            onClick={() => router.push("#")}
+            size="lg"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
+            fontSize="20px"
+            value={""}
+          ></Button>
+
+          <Button
+            onClick={() => router.push("#")}
+            size="lg"
+            rounded={"full"}
+            w="300px"
+            h="50px"
+            bgColor="green.100"
+            color="green"
+            bgGradient={"linear(to-r, green.50,green.150)"}
+            fontSize="20px"
+            value={""}
+          ></Button>
         </VStack>
       </VStack>
     </Center>
