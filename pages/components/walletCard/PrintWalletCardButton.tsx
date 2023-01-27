9463f2011a2e0accea489822ea1f2583c3dc924c
zeeyeeteecar <tomtangjob@gmail.com>
2023-01-26 16:52:51 -0800

    4

--- pages/components/walletCard/PrintWalletCardButton.tsx
+++ pages/components/walletCard/PrintWalletCardButton.tsx
@@ -2,32 +2,47 @@ import React, { useEffect, useState, useRef } from "react";
 import { Text, Button, Tag, Center, VStack } from "@chakra-ui/react";
 import { jsPDF } from "jspdf";
 
-export default function PrintButton({ application }: any ) {
+export default function PrintButton({ application }: any) {
   //const permitType:string = (application.permitType);
-
-
 
   function GeneratePDF(appl) {
     //alert(application);
     //const doc = new jsPDF();
-    
-    const doc = new jsPDF("p", "mm", [83, 83]);
-    doc.text("Richmond Centre for Disaibility", 10, 5);
+
+    const doc = new jsPDF("l", "mm", [83, 38]);
+
     //alert((rcdPermitId))
-    const permitID: string = application.permit
-      ? application.permit.rcdPermitId.toString()
+    const permitID: string = appl.permit
+      ? appl.permit.rcdPermitId.toString()
       : "N/A";
     const applicantName = appl.firstName + " " + appl.lastName;
-    doc.text("permit# " + permitID, 10, 10);
-    doc.text(applicantName, 60, 50);
+    const expiryDate = appl.permit
+      ? application.permit.expiryDate.substring(0, 7)
+      : "N/A";
+
+    const Mob = application.applicant
+      ? application.applicant.dateOfBirth.substring(0, 7)
+      : "N/A";
+      const userNo = application.applicant ? application.applicant.id : "N/A";
+
+    doc.setFontSize(15);
+    doc.text("Richmond Centre for Disaibility", 5, 10);
+
+    doc.setFontSize(10);
+    doc.text("permit # " + permitID, 5, 15);
+    doc.text("xpiry: " + expiryDate, 45, 15);
+    doc.text("Name: " + applicantName, 5, 20);
+    doc.text("MoB: " + Mob, 5, 25);
+    doc.text("User # " + userNo, 45, 25);
+    doc.text("604.232.2404 parkingpermit@rcdrichmond.org", 7, 30);
+
     doc.save("a4.pdf"); // will save the file in the current working directory
   }
-
 
   let bgclr = "";
   let fontclr = "";
   let btnDisabled = null;
-  const permitType:string = application?application.permitType:""
+  const permitType: string = application ? application.permitType : "";
 
   if (permitType === "PERMANENT") {
     bgclr = "green.100";
